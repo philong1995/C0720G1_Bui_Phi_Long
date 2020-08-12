be58a47ed2d966057ae1f95133f2ab3103c7d02d
philong1995 <long.01.04.1995@gmail.com>
2020-08-12 17:47:57 +0700

    bai_tap

---
+++ module1/7.Bien_kieu_du_lieu/thuc_hanh/js.js
@@ -0,0 +1,8 @@
+let inputweigh = prompt('enter the weigh');
+let inputheight = prompt('enter the height');
+document.write('Enter weigh: '+inputweigh);
+document.write('<br>');
+document.write('Enter height: '+inputheight);
+document.write('<br>');
+let arena = inputweigh * inputheight;
+document.write('There arena is '+arena);
